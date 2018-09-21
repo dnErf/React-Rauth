3d2809ef08c59655f75451d4e2f4d0f386fa9813
dnErf <frend.fernando@live.com>
2018-09-21 09:39:25 -0700

    update config

---
+++ config.js
@@ -0,0 +1,6 @@
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./config/keys-prod')
+}
+else {
+  module.exports = require('./config/keys-dev')
+}
---
+++ config/keys-prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  db : process.env.MONGO_URI ,
+  secrets : process.env.SECRETS
+}
